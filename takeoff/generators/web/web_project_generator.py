c8518ebd216d541f360395d40d2e722025dd7189
Marcelo Ribeiro <themarceloribeiro@gmail.com>
2021-06-08 12:16:00 -0300

    Adding support for boolean,

--- takeoff/generators/web/web_project_generator.py
+++ takeoff/generators/web/web_project_generator.py
@@ -1,6 +1,7 @@
 import os
 from jinja2 import Template
 from ..generator_base import GeneratorBase
+from pathlib import Path
 
 class WebProjectGenerator(GeneratorBase):
     def __init__(self, name, options):
@@ -10,25 +11,26 @@ class WebProjectGenerator(GeneratorBase):
         return 'web'
 
     def run(self):
+        print(f"Running Web Project Generator: {self.name}")        
         self.setup()
-        print(f"Running Web Project Generator: {self.name}")
         self.create_structure_folders()
         self.create_django_project()
         self.prepare_settings()
+        self.prepare_urls()
         self.migrate()
         self.create_admin()
     
     def migrate(self):
-        os.system(f"cd {self.project_folder()} && python3 manage.py migrate")
+        os.system(f"cd {self.project_folder()} && {self.python} manage.py migrate")
 
     def create_django_project(self):
         print('Creating Django Project')
         os.system(f"cd dist/{self.name}/web && django-admin startproject {self.name}")
-        os.system(f"cd dist/{self.name}/web/{self.name} && python3 manage.py startapp main")
+        os.system(f"cd dist/{self.name}/web/{self.name} && {self.python} manage.py startapp main")
 
     def create_structure_folders(self):
         fullpath = f"dist/{self.name}/web/"
-        print(f"    Creating Web Folder: {fullpath}")            
+        print(f"    Creating Web Folder: {fullpath}")
         os.system(f"mkdir -p {fullpath}")
     
     def prepare_settings(self):
@@ -39,20 +41,58 @@ class WebProjectGenerator(GeneratorBase):
         
         with open(settings_file, 'w') as file:
             file.writelines(lines)
-    
+
     def installed_apps_last_line(self, lines):
-        i = 0
-        start = 0
+        started = False
         finish = 0
 
-        for line in lines:
-            i += 1
+        for index, line in enumerate(lines):
             if 'INSTALLED_APPS' in line:
-                start = i
-            if start > 0 and finish == 0 and ']' in line:
-                finish = i
+                started = True
+            if started > 0 and finish == 0 and ']' in line:
+                finish = index
 
         return finish
-    
+
+    def generate_main_urls(self):
+        template_path = f"{self.templates_path}/web/urls.template"
+        destination = f"dist/{self.name}/web/{self.name}/main/urls.py"
+
+        with open(template_path) as f:
+            template_contents = f.read()
+
+        template = Template(template_contents)
+        contents = template.render(generator=self, app_name='main')
+        
+        with open(destination, 'w') as f:
+            f.write(contents)
+
+    def prepare_urls(self):
+        self.generate_main_urls()
+        urls_file = f"dist/{self.name}/web/{self.name}/{self.name}/urls.py"
+        lines = list(open(urls_file, 'r'))
+
+        for index, line in enumerate(lines):
+            if line == "from django.urls import path\n":
+                lines[index] = line.replace('path', 'path, include')
+
+        last_line = self.urls_last_line(lines)
+        lines.insert(last_line - 1, f"    path('', include('main.urls')),\n")
+
+        with open(urls_file, 'w') as file:
+            file.writelines(lines)
+
+    def urls_last_line(self, lines):
+        started = False
+        finish = 0
+
+        for index, line in enumerate(lines):
+            if 'urlpatterns' in line:
+                started = True
+            if started and finish == 0 and ']' in line:
+                finish = index
+
+        return finish
+
     def create_admin(self):
-        os.system(f"cd {self.project_folder()} && python3 manage.py createsuperuser")
+        os.system(f"cd {self.project_folder()} && {self.python} manage.py createsuperuser")
